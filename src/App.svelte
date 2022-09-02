c4f8e1e4cca259e8cc2447dfc490e94d9356c158
Leo-paul <leopaulmartin@hotmail.fr>
2022-09-02 23:41:02 +0200

    update head meta data

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,31 @@
 <script>
-	import svelteLogo from "./assets/svelte.svg";
 	import WordleList from "./lib/WordleList.svelte";
 </script>
+
+<head
+	><!-- Primary Meta Tags -->
+	<title>World of Wordle</title>
+	<meta name="title" content="World of Wordle" />
+	<meta name="description" content="All the wordle games you can play today!" />
+	<meta name="”robots”" content="index, follow" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta charset="UTF-8" />
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="World of Wordle" />
+	<meta
+		property="og:description"
+		content="All the wordle games you can play today!"
+	/>
+
+	<!-- Twitter -->
+	<meta property="twitter:title" content="World of Wordle" />
+	<meta
+		property="twitter:description"
+		content="All the wordle games you can play today!"
+	/>
+</head>
 
 <main>
 	<h1>World of Wordle</h1>

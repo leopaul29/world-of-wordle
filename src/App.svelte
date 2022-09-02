2b8d3a5a6e0a1f7aa6ea09d95a730fc15034150c
Leo-paul <leopaulmartin@hotmail.fr>
2022-09-02 23:05:13 +0200

    create worlde list component

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,23 @@
 <script>
 	import svelteLogo from "./assets/svelte.svg";
-	import Counter from "./lib/Counter.svelte";
+	import WordleList from "./lib/WordleList.svelte";
 </script>
 
 <main>
-	<h1>World of Wordle</h1>
-
 	<a
 		href="https://www.nytimes.com/games/wordle/index.html"
 		target="_blank"
 		rel="noopener noreferrer"
+		class="titleLink"
 	>
-		<button> New York Times Wordle </button>
+		<h1>World of Wordle</h1>
 	</a>
+
+	<WordleList />
 </main>
 
 <style>
+	.titleLink {
+		color: rgba(255, 255, 255, 0.87);
+	}
 </style>
---
+++ src/lib/WordleList.svelte
@@ -0,0 +1,20 @@
+<script>
+	import data from "../data/wordlelist.json";
+
+	console.log(data);
+</script>
+
+<div class="wordleList">
+	{#each data as wordle}
+		<a href={wordle.link} target="_blank" rel="noopener noreferrer">
+			<button>{wordle.label}</button>
+		</a>
+	{/each}
+</div>
+
+<style>
+	.wordleList {
+		max-width: 960px;
+		padding: 40px;
+	}
+</style>
